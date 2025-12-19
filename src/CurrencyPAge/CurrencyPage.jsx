e24cbcb8779c8aed20e429d9820072d07c9587da
nyapblesso-a11y <nyapblesso@gmail.com>
2025-12-19 11:11:27 +0100

    the ui of the currency exchange wallet has been completed

--- src/CurrencyPAge/CurrencyPage.jsx
+++ src/CurrencyPAge/CurrencyPage.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import Header from '../Components/HeaderComponent/Header'
-import "./currency.css"
-import Balance from '../Components/BalancePAge/Balance'
-import FundsPge from '../Components/FundsPage/FundsPge'
+import React from "react";
+import Header from "../Components/HeaderComponent/Header";
+import "./currency.css";
+import Balance from "../Components/BalancePAge/Balance";
+import FundsPge from "../Components/FundsPage/FundsPge";
 function CurrencyPage() {
   return (
     <>
-     <div className='main-bord'>
-        <Header/>
-        <Balance/>
-        <FundsPge/>
-     </div>
+      <div className="main-bord">
+        <Header />
+        <Balance />
+        <FundsPge />
+        <hr />
+        <div className="total-value">
+          <h4>
+            Total Value in USD: <span>$736.38 USD</span>
+          </h4>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default CurrencyPage
+export default CurrencyPage;
